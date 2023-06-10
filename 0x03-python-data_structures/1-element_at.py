1f85e674a3fa7555e59897b14f77f2e13934cc13
Victor Abuka <vickerdent@gmail.com>
2023-06-10 22:47:19 +0000

    Upload corrections

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx > len(my_list):
-        return None
-    if idx < 0:
+    if (my_list is None) or (idx is None)\
+       or (idx < 0) or (idx >= len(my_list)):
         return None
     return my_list[idx]
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        return None
     val = len(my_list) - 1
     while val >= 0:
         print("{:d}".format(my_list[val]))
