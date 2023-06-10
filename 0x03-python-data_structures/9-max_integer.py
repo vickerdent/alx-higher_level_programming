c5a39f17d223f57210986de41b2f1cf4316fbcd3
Victor Abuka <vickerdent@gmail.com>
2023-06-10 23:08:58 +0000

    correct mistake of 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,14 +2,8 @@
 def max_integer(my_list=[]):
     if my_list is None or len(my_list) == 0:
         return None
-    prev = 0
-    curr = 0
-    big = 0
-    for num in my_list:
-        curr = num
-        if curr > prev:
-            big = curr
-            prev = curr
-        else:
-            big = prev
-    return big
+    max = my_list[0]
+    for i in my_list:
+        if max < i:
+            max = i
+    return max
