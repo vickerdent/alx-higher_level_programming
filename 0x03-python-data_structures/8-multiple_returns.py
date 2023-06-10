d7d601e11a246c956b43c70b0ea004d5e21e52d8
Victor Abuka <vickerdent@gmail.com>
2023-06-10 23:04:25 +0000

    upload again

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0 or sentence is None:
-        return None
+    if sentence is None or len(sentence) == 0:
+        return (0, None)
     total = len(sentence)
     first_letter = sentence[0]
     return total, first_letter
