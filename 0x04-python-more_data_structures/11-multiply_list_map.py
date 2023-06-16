2672f92117a6be98a87f21c8ff540a62853b3282
Victor Abuka <vickerdent@gmail.com>
2023-06-16 11:05:16 +0000

    Restructure map object

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    new_list = [*map(lambda x: x * number, my_list)]
-    return new_list
+    return [*map(lambda x: x * number, my_list)]
