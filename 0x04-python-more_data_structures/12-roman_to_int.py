0b367b252295d153c06122b545cfa8253e6fe0e6
Victor Abuka <vickerdent@gmail.com>
2023-06-16 11:11:54 +0000

    Process bugs in files 12 and 14

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or \
-       roman_string  is None:
+    if roman_string is None or type(roman_string) is not str:
         return 0
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                  'C': 100, 'D': 500, 'M': 1000}
-    result = 0
-    for i in range(len(roman_string)):
-        if i > 0 and roman_dict[roman_string[i]] > \
-           roman_dict[roman_string[i - 1]]:
-            result += roman_dict[roman_string[i]] - 2 * \
-                roman_dict[roman_string[i - 1]]
-        else:
-            result += roman_dict[roman_string[i]]
-    return result
+    sumtot = 0
+    prevval = 1000
+    romandict = {"I": 1, "V": 5, "X": 10, "L": 50,
+                 "C": 100, "D": 500, "M": 1000}
+    numbers = [romandict[x] for x in roman_string]
+    for x in numbers:
+        if x > prevval:
+            sumtot -= prevval * 2
+        prevval = x
+        sumtot += x
+    return sumtot
