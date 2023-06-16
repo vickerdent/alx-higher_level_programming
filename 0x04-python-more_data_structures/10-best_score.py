e76272404338d1de59e2490ff5c48f7e367e302b
Victor Abuka <vickerdent@gmail.com>
2023-06-16 11:02:56 +0000

    Handled dictionary structure

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
+    if a_dictionary is not None and len(a_dictionary) > 0:
         prev = 0
         prev_key = ""
         curr = 0
