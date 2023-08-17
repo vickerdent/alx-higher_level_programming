a3c67599a39b7524092d733d265eb4a6b5af5281
Victor Abuka <vickerdent@gmail.com>
2023-08-17 07:40:41 +0100

    Correct bugged tasks

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- show grants for two users
--- show grants for user_0d_1
-SHOW GRANTS FOR user_0d_1@localhost;
--- show grants for user_0d_2
-SHOW GRANTS FOR user_0d_2@localhost;
+-- show grants for user_0d_1 and user_0d_2 in localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
